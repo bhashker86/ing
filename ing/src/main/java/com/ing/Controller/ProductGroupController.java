c569440f560feae08c72b6126386567a3653fbb0
bhashker <bhashker@DESKTOP-0965RUV>
2019-04-06 20:12:12 +0530

    to master branch

---
+++ ing/src/main/java/com/ing/Controller/ProductGroupController.java
@@ -0,0 +1,33 @@
+package com.ing.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ing.domain.ProductGroup;
+import com.ing.service.ProductGroupService;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@CrossOrigin
+@RestController
+@EnableSwagger2
+@RequestMapping("api/v1/")
+//@RequestMapping("13.233.208.167:9080/bankING/api/v1/")
+public class ProductGroupController {
+
+	@Autowired
+	ProductGroupService productGroupService;
+
+	@GetMapping("/groups")
+	public ResponseEntity<List<ProductGroup>> getGroups() {
+		List<ProductGroup> list = productGroupService.getAllProductGroups();
+		return ResponseEntity.ok().body(list);
+	}
+
+}
---
+++ ing/src/main/java/com/ing/domain/Product.java
@@ -0,0 +1,68 @@
+package com.ing.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+	@Id
+	@GeneratedValue
+	private Long productId;
+	private String productName;
+	private String product_type;
+	private Long balance;
+	private Long max_limit;
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProduct_type() {
+		return product_type;
+	}
+
+	public void setProduct_type(String product_type) {
+		this.product_type = product_type;
+	}
+
+	public Long getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Long balance) {
+		this.balance = balance;
+	}
+
+	public Long getMax_limit() {
+		return max_limit;
+	}
+
+	public void setMax_limit(Long max_limit) {
+		this.max_limit = max_limit;
+	}
+
+	public Product() {
+		super();
+	}
+
+	public Product(Long productId) {
+		super();
+		this.productId = productId;
+	}
+}
